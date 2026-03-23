10d0ac23daea25a8bbcd2458c0da58450a0955c1
amardipcsit25-crypto <amardipcsit25@oic.edu.np>
2026-03-23 22:28:27 +0545

    Delete singh.c

--- singh.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a, b;
-    printf("enter the two numbers that you want to add: ");
-    scanf("%d %d", &a, &b);
-    printf("the sum of %d and %d is %d", a, b, a+b);
-    return 0;
-}
