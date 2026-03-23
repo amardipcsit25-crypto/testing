494fd0910e3a28d6013fb2ab258db945c239bf6d
Amardip Singh <Amardipcsit25@oic.edu.np>
2026-03-23 22:35:10 +0545

    added file

---
+++ singh.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter the two numbers that you want : ");
+    scanf("%d %d", &a, &b);
+    printf("the product of %d and %d is %d", a, b, a*b);
+    return 0;
+}
