7afecbb9239596064e23886a1d2c35b4a391f3e1
Amardip Singh <Amardipcsit25@oic.edu.np>
2026-03-25 09:23:10 +0545

    new code

--- amar.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a, b;
-    printf("enter the two numbers: ");
-    scanf("%d %d", &a, &b);
-    printf("the product of %d and %d is %d", a, b, a*b);
-    return 0;
-}
