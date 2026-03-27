db74b11aeef375d8e3546daa1431e75138772e79
amardipcsit25-crypto <amardipcsit25@oic.edu.np>
2026-03-27 19:39:29 +0545

    Delete b.c

--- b.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-        int num1, num2, difference;
-    printf("Enter two numbers that you want: ");
-    scanf("%d %d", &num1, &num2);
-    difference = num1 - num2;
-    printf("Difference: %d\n", difference);
-    return 0;
-}
