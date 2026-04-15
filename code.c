03cfa421b53b5fd5332bdbc60c817445a3d5b9f7
Amardip Singh <Amardipcsit25@oic.edu.np>
2026-04-15 16:46:27 +0545

    code on the b

--- code.c
+++ code.c
@@ -2,8 +2,7 @@
 int main()
 {
     int a,b;
-    printf("Enter two numbers that you want:");
+    printf("Enter two numbers: ");
     scanf("%d%d",&a,&b);
-    printf("The difference of %d and %d is %d",a,b,a-b);
     printf("The sum of %d and %d is %d",a,b,a+b);
     return 0;}
